87bcd0f730e7f28863cc7253c6231ce47bdd9218
Avery.Davenport <averymdavenport@gmail.com>
2021-07-02 15:24:18 -0600

    commit 3

--- writeCode.js
+++ writeCode.js
@@ -1,19 +1,19 @@
-// function findLongestWord(arr){
-//     let answer = [''];
-//     for(i=0; i<arr.length; i++){
-//       if (answer[0].length < arr[i].length) {
-//         answer.splice(0,1,arr[i])
-//       }
-//     }
-//     return answer
-//   };
+// //Finds the longest word in an array
+function findLongestWord(arr){
+    let answer = [''];
+    for(i=0; i<arr.length; i++){
+      if (answer[0].length < arr[i].length) {
+        answer.splice(0,1,arr[i])
+      }
+    }
+    return answer
+  };
 
-// let wordArray = ['hello', 'Avery', 'Longestword', 'actuallongestword', 'Thisisnowthelongestwordinthearray']
+let wordArray = ['hello', 'Avery', 'Longestword', 'actuallongestword', 'Thisisnowthelongestwordinthearray']
 
-// console.log(findLongestWord(wordArray))
+console.log(findLongestWord(wordArray))
 
- //Finds the longest word in an array
-
+// //Check if any numbers in an array add to 0
 function addToZero(numArray){
     for(i=0; i<numArray.length; i++){
         for(j=i+1; j<numArray.length; j++){
@@ -25,22 +25,24 @@ function addToZero(numArray){
     return false
 };
 
-// function addToZero(numArray){
-//     if (numArray.length < 2) {
-//         return false
-//     };
-
-//     for(i=0; i<numArray.length; i++){
-//          for(let j=i+1; j<numArray.length; j++){
-//             if (numArray[i] + numArray[j] === 0) {
-//                 return true
-//                 }
-//              }
-//         }
-//     return false
-// }
-
 console.log(addToZero([]))
 console.log(addToZero([2,4,-2]))
 console.log(addToZero(5,2,0,0,7))
 console.log(addToZero([7,8.6,-7]))
+
+//Check if all letters in a string are unique
+function hasUniqueChars(string){
+    stringArray = string.toLowerCase().replace(/\s/g, "").split('')
+    // console.log(stringArray)
+    for (i=0; i<stringArray.length; i++){
+        for (j=i+1; j<stringArray.length; j++){
+            if(stringArray[i] === stringArray[j]){
+                return true
+            }
+        }
+    }
+    return false
+};
+
+console.log(hasUniqueChars("Monday"));
+console.log(hasUniqueChars("Moonday"));
